96da0e6d1cf7daab1495b02f826ba15090358496
Dominik Porębski <dp305922@student.polsl.pl>
2025-11-26 17:26:49 +0100

    fix(client): Dodano brakujący using i zainstalowano brakującą bibliotekę

--- src/Nexa.Client/App.xaml.cs
+++ src/Nexa.Client/App.xaml.cs
@@ -17,6 +17,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
